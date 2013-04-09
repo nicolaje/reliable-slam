4c0c04c2fa3df7ec3f8051dc15fd14bcd2572e08
Jeremy Nicola <jeremy.nicola@gmail.com>
2013-04-09 18:00:27 -0300

    Fixed data-logging, it looks OK (plotted it on SciLab). Have to check with higher std-deviation to be sure.

--- workspace/Simulations/Scenarios/2D-2Transponders/data_logger.py
+++ workspace/Simulations/Scenarios/2D-2Transponders/data_logger.py
@@ -26,7 +26,6 @@ def update_pose_noisy(data):
 
 def update_imu_pure(data):
 	global _imu_pure
-	print(data)
 	_imu_pure=data
 
 def update_imu_noisy(data):
@@ -56,17 +55,20 @@ if(len(sys.argv)>3):
 	duration=int(sys.argv[2]) # Logging duration in seconds
 	frequency=int(sys.argv[3])# Loggin frequency in Hz
 	f=open(str(file_name), 'w')
+	
 	# Data format:
-	# pose_pure.x; pose_pure.y; pose_pure.z; pose_pure.yaw; pose_pure.pitch; pose_pure.roll;
-	# pose_noisy.x; pose_noisy.y; pose_noisy.z; pose_noisy.yaw; pose_noisy.pitch; pose_noisy.roll;
-	# imu_pure.dx; imu_pure.dy; imu_pure.dz;
-	# imu_noisy.dx; imu_noisy.dy; imu_noisy.dz;
-	# imu_pure.dtheta; imu_pure.dphi; imu_pure.dpsi;
-	# imu_noisy.dtheta; imu_noisy.dphi; imu_noisy.dpsi;
-	# transponder1.pure; transponder2.pure;
-	# transponder1.pure; transponder2.noisy;
-	# loch_doppler_pure.vx; loch_doppler_pure.vy; loch_doppler_pure.vz;
-	# loch_doppler_noisy.vx; loch_doppler_noisy.vy; loch_doppler_noisy.vz;
+	f.write('# pose_pure.x; pose_pure.y; pose_pure.z;'+\
+	'pose_noisy.x; pose_noisy.y; pose_noisy.z;'\
+	'pose_pure.yaw; pose_pure.pitch; pose_pure.roll;'\
+	'pose_noisy.yaw; pose_noisy.pitch; pose_noisy.roll;'\
+	'imu_pure.d²x; imu_pure.d²y; imu_pure.d²z;'\
+	'imu_noisy.d²x; imu_noisy.d²y; imu_noisy.d²z;'\
+	'imu_pure.dtheta; imu_pure.dphi; imu_pure.dpsi;'\
+	'imu_noisy.dtheta; imu_noisy.dphi; imu_noisy.dpsi;'\
+	'transponder1.pure; transponder2.pure;'\
+	'transponder1.pure; transponder2.noisy;'\
+	'loch_doppler_pure.vx; loch_doppler_pure.vy; loch_doppler_pure.vz;'\
+	'loch_doppler_noisy.vx; loch_doppler_noisy.vy; loch_doppler_noisy.vz;\n')
 	with pymorse.Morse() as simu:
 		sub=simu.sub
 		sub.pose_pure.subscribe(update_pose_pure)
@@ -83,16 +85,40 @@ if(len(sys.argv)>3):
 			print('T: %i'%delta)
 			now=datetime.now()
 			delta=(now-time).seconds+(now-time).microseconds/1000000
+			
 			# Concatenate data
 			if (_pose_pure!=0) and (_pose_noisy!=0) and (_imu_pure!=0)\
 			and (_imu_noisy!=0) and (_pinger_pure!=0) and (_pinger_noisy!=0)\
 			and (_loch_doppler_pure!=0) and (_loch_doppler_noisy!=0):
 				s=str(_pose_pure['x'])+';'+str(_pose_pure['y'])+';'+str(_pose_pure['z'])+';'
-				s=s+str(_pose_pure['yaw'])+';'+str(_pose_pure['pitch'])+';'+str(_pose_pure['roll'])+';'
 				s=s+str(_pose_noisy['x'])+';'+str(_pose_noisy['y'])+';'+str(_pose_noisy['z'])+';'
+				s=s+str(_pose_pure['yaw'])+';'+str(_pose_pure['pitch'])+';'+str(_pose_pure['roll'])+';'
 				s=s+str(_pose_noisy['yaw'])+';'+str(_pose_noisy['pitch'])+';'+str(_pose_noisy['roll'])+';'
-#				s=s+str(_imu_pure[''])
-				print(s)
+				
+				linear_acceleration_pure=_imu_pure['linear_acceleration']
+				linear_acceleration_noisy=_imu_noisy['linear_acceleration']
+				
+				angular_velocity_pure=_imu_pure['angular_velocity']
+				angular_velocity_noisy=_imu_noisy['angular_velocity']
+				
+				s=s+str(linear_acceleration_pure[0])+";"+str(linear_acceleration_pure[1])+";"+str(linear_acceleration_pure[2])+";"
+				s=s+str(linear_acceleration_noisy[0])+";"+str(linear_acceleration_noisy[1])+";"+str(linear_acceleration_noisy[2])+";"
+				s=s+str(angular_velocity_pure[0])+";"+str(angular_velocity_pure[1])+";"+str(angular_velocity_pure[2])+";"
+				s=s+str(angular_velocity_noisy[0])+";"+str(angular_velocity_noisy[1])+";"+str(angular_velocity_noisy[2])+";"
+				
+				transponders_pure=_pinger_pure['near_objects']
+				transponders_noisy=_pinger_noisy['near_objects']
+				
+				s=s+str(transponders_pure['transponder1'])+";"
+				s=s+str(transponders_noisy['transponder2'])+";"
+				
+				loch_doppler_pure=_loch_doppler_pure['linear_velocity']
+				loch_doppler_noisy=_loch_doppler_noisy['linear_velocity']
+				
+				s=s+str(loch_doppler_pure[0])+";"+str(loch_doppler_pure[1])+";"+str(loch_doppler_pure[2])+";"
+				s=s+str(loch_doppler_noisy[0])+";"+str(loch_doppler_noisy[1])+";"+str(loch_doppler_noisy[2])+";"
+				
+				f.write(s+'\n')
 			else:
 				print("+++++++++++++++++++++++++++++++++")
 				print("+++++++++++++++++++++++++++++++++")
