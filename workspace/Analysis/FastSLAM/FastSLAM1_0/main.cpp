d982284cddb5595e2d045abf77cbf9ec896674a4
Jeremy Nicola <jeremy.nicola@gmail.com>
2013-07-16 11:38:45 -0300

    Init. FastSLAM

--- workspace/Analysis/FastSLAM/FastSLAM1_0/main.cpp
+++ workspace/Analysis/FastSLAM/FastSLAM1_0/main.cpp
@@ -7,6 +7,9 @@
 #include "../../General/Utils/morsedataparser.h"
 #include "../../General/Utils/robot.h"
 #include "particle.h"
+#include "fastslam.h"
+
+using namespace Eigen;
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +20,27 @@ int main(int argc, char *argv[])
 
     MORSEDataParser p("/home/jem/reliable-slam/workspace/Simulations/Scenarios/2D-4Transponders/2D-4Transponders-Circle.res",1,4);
     Robot r=p.nextRecord()[0];
+
+    Matrix3d positionCovariance,orientationCovariance,linearMotionCovariance,angularMotionCovariance;
+    positionCovariance
+            << 2.5,0,0,
+            0,2.5,0,
+            0,0,0.15;
+    orientationCovariance
+            << 0.02*M_PI/180,0,0,
+            0,0.02*M_PI/180,0,
+            0,0,0.02*M_PI/180;
+    linearMotionCovariance
+            << 0.04,0,0,
+            0,0.04,0,
+            0,0,0.04;
+    angularMotionCovariance
+            << 0.01*M_PI,0,0,
+            0,0.01*M_PI,0,
+            0,0,0.01*M_PI;
+    double pingerVariance=0.03;
+
+    FastSLAM estimator;
 
     return a.exec();
 }
