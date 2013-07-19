8eb91ce1c94deacf1d79b8dd90edd20ab5cef39e
Jeremy Nicola <jeremy.nicola@gmail.com>
2013-07-18 22:40:39 -0300

    Spotted another squaredNorm() instead of norm() in the jacobian of the obs. model

--- workspace/Analysis/FastSLAM/FastSLAM1_0/kalmanfilter.cpp
+++ workspace/Analysis/FastSLAM/FastSLAM1_0/kalmanfilter.cpp
@@ -16,30 +16,19 @@ double KalmanFilter::update(double distance, Vector3d robotPos)
 {
     double weight=0;
     double zHat=observation(robotPos);
-    std::cout << "distance: "<<distance<<std::endl;
-    std::cout << "zHat: "<<zHat <<std::endl;
     double delta=(distance-zHat);
-    std::cout <<"Delta: "<<delta<<std::endl;
     RowVector3d H=jacobianObservationModel(robotPos);
-    std::cout <<"H: "<<H<<std::endl;
     double Q=H*covariance*(H.transpose())+pingerVariance;
-    std::cout << "Q: "<<Q<<std::endl;
     Vector3d K=(1/Q)*covariance*(H.transpose());
-    std::cout <<"Kalman Gain: "<<K.transpose()<<std::endl;
-    //mean=mean+K*delta;
-    std::cout << "K*delta: " <<K*delta<<std::endl;
+    mean=mean+K*delta;
     covariance=(Matrix<double,3,3>::Identity()-K*H)*covariance;
     weight=(1/sqrt(2*M_PI*abs(Q)))*exp((-0.5/Q)*pow(delta,2));
-    std::cout << "weight returned after KF update: "<<weight<<std::endl;
     return weight;
 }
 
 double KalmanFilter::observation(Vector3d robotPosition)
 {
-    std::cout << "mean: " <<mean<<std::endl;
-    std::cout << "robotPosition: "<<robotPosition<<std::endl;
     Vector3d v=mean-robotPosition;
-    std::cout << "v: " <<v<<std::endl;
     return (mean-robotPosition).norm();
 }
 
@@ -48,8 +37,6 @@ RowVector3d KalmanFilter::jacobianObservationModel(Vector3d robotPosition)
 {
     RowVector3d res;
     Vector3d tmp=(mean-robotPosition);
-    std::cout<<"Tmp: "<<tmp.transpose()<<std::endl;
-    res=(tmp/tmp.squaredNorm()).transpose(); // TODO test if it works
-    std::cout<<"Res: "<<res<<std::endl;
+    res=(tmp/tmp.norm()).transpose();
     return res;
 }
